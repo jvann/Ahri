c9afc3ba9c6675b7d554281583ef1dd47ea70067
Jibril <j.d.ivan69@gmail.com>
2017-12-22 13:30:13 -0600

    event_row finished.

---
+++ AhriProject/app/src/main/java/com/jibril/ahri/AgendaEventAdapter.java
@@ -0,0 +1,50 @@
+package com.jibril.ahri;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by jibril on 12/22/17.
+ */
+
+public class AgendaEventAdapter extends ArrayAdapter<Event> {
+
+    private Context context;
+
+    public AgendaEventAdapter(Context context, ArrayList<Event> events) {
+        super(context, 0, events);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.event_row, parent, false);
+        }
+
+        //Variables
+        TextView tv_event_name = (TextView) convertView.findViewById(R.id.text_event_name);
+        TextView tv_event_datetime = (TextView) convertView.findViewById(R.id.text_event_datetime);
+        ImageView iv_event_status = (ImageView) convertView.findViewById(R.id.image_event_status);
+        ImageView iv_event_divider = (ImageView) convertView.findViewById(R.id.image_divider);
+
+        //Get event item
+        Event event = getItem(position);
+
+        //Set elements
+        tv_event_name.setText("getName()");
+        tv_event_datetime.setText("getDateTime");
+//        iv_event_status.setImageResource(R.mipmap.ic_launcher_round);
+//        iv_event_divider.setImageResource(R.raw.divider_diamond);
+
+        return convertView;
+    }
+
+}
--- AhriProject/app/src/main/java/com/jibril/ahri/AgendaFragment.java
+++ AhriProject/app/src/main/java/com/jibril/ahri/AgendaFragment.java
@@ -3,27 +3,58 @@ package com.jibril.ahri;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AgendaFragment extends Fragment {
+public class AgendaFragment extends ListFragment {
 
+    private AgendaEventAdapter adapterEvents;
+    private ArrayList<Event> listEvents;
 
     public AgendaFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        listEvents = getEvents();
+        //TODO: Delete, just for debug
+        adapterEvents = new AgendaEventAdapter(this.getContext(), listEvents);
+        setListAdapter(adapterEvents);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_agenda, container, false);
+    }
+
+    //TODO: Delete after databae
+    private ArrayList<Event> getEvents() {
+        //Log to remind deletion.
+        Log.w("REMOVE", "Remove getEvents after database creation.");
+        ArrayList<Event> listEvent = new ArrayList<Event>();
+        Event e = new Event("Success", "Wed", "1pm");
+        listEvent.add(e);
+
+        return listEvent;
     }
 
 }
