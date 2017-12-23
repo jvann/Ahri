cbbb551409c839615becec3d722af4738449c3b0
Jibril <j.d.ivan69@gmail.com>
2017-12-22 23:11:59 -0600

    Added functionaility to FAB elements in AgendaFragment and FinanceFragment.

--- AhriProject/app/src/main/java/com/jibril/ahri/AgendaFragment.java
+++ AhriProject/app/src/main/java/com/jibril/ahri/AgendaFragment.java
@@ -2,6 +2,7 @@ package com.jibril.ahri;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
@@ -15,10 +16,14 @@ import java.util.ArrayList;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AgendaFragment extends ListFragment {
+public class AgendaFragment extends ListFragment implements View.OnClickListener {
 
     private AgendaEventAdapter adapterEvents;
     private ArrayList<Event> listEvents;
+
+    //FAB elements
+    public FloatingActionButton fabCreateQuickEvent;
+    public FloatingActionButton fabCreateEvent;
 
     public AgendaFragment() {
         // Required empty public constructor
@@ -37,6 +42,10 @@ public class AgendaFragment extends ListFragment {
         //TODO: Delete, just for debug
         adapterEvents = new AgendaEventAdapter(this.getContext(), listEvents);
         setListAdapter(adapterEvents);
+
+        initElements();
+        startListeners();
+
     }
 
     @Override
@@ -44,6 +53,33 @@ public class AgendaFragment extends ListFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_agenda, container, false);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.fab_create_quick_event:
+                Log.d("FAB", "QuickEvent");
+                break;
+            case R.id.fab_create_event:
+                Log.d("FAB", "Event");
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void initElements() {
+
+        //FAB elements
+        fabCreateQuickEvent = (FloatingActionButton) getView().findViewById(R.id.fab_create_quick_event);
+        fabCreateEvent = (FloatingActionButton) getView().findViewById(R.id.fab_create_event);
+    }
+
+    private void startListeners() {
+        fabCreateQuickEvent.setOnClickListener(this);
+        fabCreateEvent.setOnClickListener(this);
     }
 
     //TODO: Delete after databae
--- AhriProject/app/src/main/java/com/jibril/ahri/FinanceFragment.java
+++ AhriProject/app/src/main/java/com/jibril/ahri/FinanceFragment.java
@@ -2,7 +2,9 @@ package com.jibril.ahri;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,8 +13,11 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FinanceFragment extends Fragment {
+public class FinanceFragment extends Fragment implements View.OnClickListener {
 
+    //FAB elements
+    public FloatingActionButton fabCreateQuickTransaction;
+    public FloatingActionButton fabCreateTransaction;
 
     public FinanceFragment() {
         // Required empty public constructor
@@ -26,6 +31,9 @@ public class FinanceFragment extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        initElements();
+        startListeners();
     }
 
     @Override
@@ -33,6 +41,33 @@ public class FinanceFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_finance, container, false);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.fab_create_quick_transaction:
+                Log.d("FAB", "QuickTransaction");
+                break;
+            case R.id.fab_create_transaction:
+                Log.d("FAB", "Transaction");
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void initElements() {
+
+        //FAB elements
+        fabCreateQuickTransaction = (FloatingActionButton) getView().findViewById(R.id.fab_create_quick_transaction);
+        fabCreateTransaction = (FloatingActionButton) getView().findViewById(R.id.fab_create_transaction);
+    }
+
+    private void startListeners() {
+        fabCreateQuickTransaction.setOnClickListener(this);
+        fabCreateTransaction.setOnClickListener(this);
     }
 
 }
